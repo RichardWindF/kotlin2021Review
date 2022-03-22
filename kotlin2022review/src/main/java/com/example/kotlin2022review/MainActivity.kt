16863d65fae72132c20c6945900284e55c059cfa
RichardWindF <romewind1@gmail.com>
2022-03-22 17:14:00 -0400

    Kotlin Review widges, layout

--- kotlin2022review/src/main/java/com/example/kotlin2022review/MainActivity.kt
+++ kotlin2022review/src/main/java/com/example/kotlin2022review/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.kotlin2022review
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.comps_linearlayout)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(View.OnClickListener {
+            view ->  button.setText("I love Canada！")
+        })
     }
 }
