238428705e5cfedcecc5edc740e31300d836dea8
RichardWindF <romewind1@gmail.com>
2022-03-26 13:41:47 -0400

    Kotlin Review RecyclerView

--- kotlin2022review/src/main/java/com/example/kotlin2022review/MainActivity.kt
+++ kotlin2022review/src/main/java/com/example/kotlin2022review/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.kotlin2022review
 
 import android.annotation.SuppressLint
+import android.icu.lang.UCharacter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButtonToggleGroup
 
 class MainActivity : AppCompatActivity()
@@ -15,15 +18,15 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(R.layout.comps_material_button)
+        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.comps_material_button)
 
         /* val button = findViewById<Button>(R.id.button)
          button.setOnClickListener(View.OnClickListener {
              view ->  button.setText("I love Canada！")
          })*/
 
-        val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleGroup)
+        /*val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleGroup)
         toggleGroup.addOnButtonCheckedListener(object :
             MaterialButtonToggleGroup.OnButtonCheckedListener
         {
@@ -38,6 +41,12 @@ class MainActivity : AppCompatActivity()
                 Log.e(TAG, "onButtonChecked: " + checkedId)
             }
 
-        })
+        })*/
+        /*ActivityMainBinding
+        linear_rv.*/
+        val mRecyclerView = findViewById<RecyclerView>(R.id.linear_rv)
+       // mRecyclerView.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)    //纵向
+        mRecyclerView.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)  //横向
+        mRecyclerView.adapter=MyAdapter()
     }
 }
